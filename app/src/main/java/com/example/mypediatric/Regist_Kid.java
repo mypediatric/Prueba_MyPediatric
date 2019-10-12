a8818576f5958491c6cf114fb1c55b7d9fae31bc
DanFelNix8 <dpipe828@gmail.com>
2019-10-12 00:18:02 -0500

    Registro niños - cambiar diseño deacuerdo al diseño de laura...

---
+++ app/src/main/java/com/example/mypediatric/Regist_Kid.java
@@ -0,0 +1,36 @@
+package com.example.mypediatric;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class Regist_Kid extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+
+        txtAdress = findViewById(R.id.txtAdress);
+        txtBirth = findViewById(R.id.txtBirth);
+        txtDocNum = findViewById(R.id.txtDocNum);
+        txtDocType = findViewById(R.id.txtDocType);
+        txtEmail = findViewById(R.id.txtEmail);
+        txtLastName = findViewById(R.id.txtLastName);
+        txtName = findViewById(R.id.txtName);
+        txtPass = findViewById(R.id.txtPass);
+        radgChromosome23 = findViewById(R.id.radgChromosome23);
+        rbMale = findViewById(R.id.rbMale);
+        rbFemale = findViewById(R.id.rbFemale);
+        chkTandc = findViewById(R.id.chkTandc);
+        InitializeFirebase();
+
+        private void InitializeFirebase() {
+            FirebaseApp.initializeApp(this);
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            DatabaseReference = firebaseDatabase.getReference();
+        }
+    }
+}
