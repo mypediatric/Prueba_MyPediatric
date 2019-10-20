fa59305b8864d8498227611661495502db2203e0
DanFelNix8 <dpipe828@gmail.com>
2019-10-20 18:14:23 -0500

    Registro niños - FALTA AGREGAR LA INFORMACIÓN A LOS SPINNER...
    Clases_Reg_Kids - Regist_Kid conexion android y base de datos...
    
    activity_registrokids - codigo diseño
    
    Spinner_ Regist_Kid _ informacion del spinner (FALTA)
    
    No cambie nada de lo que estaba en la anterior version.

--- app/src/main/java/com/example/mypediatric/Regist_Kid.java
+++ app/src/main/java/com/example/mypediatric/Regist_Kid.java
@@ -2,32 +2,47 @@ package com.example.mypediatric;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.sql.DatabaseMetaData;
 
 public class Regist_Kid extends AppCompatActivity {
+EditText txtNombreK, txtApellidK,txtPesoK,txtEstaturaK;
+Spinner spinGen,spinRH,spinEdad;
+Button btnRegistarK;
+
+private DatabaseReference RegistKid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
-/*
-        txtAdress = findViewById(R.id.txtAdress);
-        txtBirth = findViewById(R.id.txtBirth);
-        txtDocNum = findViewById(R.id.txtDocNum);
-        txtDocType = findViewById(R.id.txtDocType);
-        txtEmail = findViewById(R.id.txtEmail);
-        txtLastName = findViewById(R.id.txtLastName);
-        txtName = findViewById(R.id.txtName);
-        txtPass = findViewById(R.id.txtPass);
-        radgChromosome23 = findViewById(R.id.radgChromosome23);
-        rbMale = findViewById(R.id.rbMale);
-        rbFemale = findViewById(R.id.rbFemale);
-        chkTandc = findViewById(R.id.chkTandc);
-        InitializeFirebase();
+        RegistKid = FirebaseDatabase.getInstance().getReference("Clases_Reg_Kids");
+        txtNombreK=(EditText) findViewById(R.id.txtNombreK);
+        txtApellidK=(EditText) findViewById(R.id.txtApellidK);
+        txtPesoK=(EditText) findViewById(R.id.txtPesoK);
+        txtEstaturaK=(EditText) findViewById(R.id.txtEstaturaK);
+        spinGen=(Spinner) findViewById(R.id.spinGen);
+        spinRH=(Spinner) findViewById(R.id.spinRH);
+        spinEdad=(Spinner) findViewById(R.id.spinEdad);
+        btnRegistarK=(Button) findViewById(R.id.btnRegistarK);
 
-        private void InitializeFirebase() {
-            FirebaseApp.initializeApp(this);
-            FirebaseDatabase database = FirebaseDatabase.getInstance();
-            DatabaseReference = firebaseDatabase.getReference();
-        }*/
     }
-}
+    public void RegistrKid(){
+        String genero=spinGen.getSelectedItem().toString();
+        String rh=spinRH.getSelectedItem().toString();
+        String edad=spinEdad.getSelectedItem().toString();
+        String nombre=txtNombreK.getText().toString();
+        String apellido=txtApellidK.getText().toString();
+        String peso=txtPesoK.getText().toString();
+        String estatura=txtEstaturaK.getText().toString();
+    }
+    }
