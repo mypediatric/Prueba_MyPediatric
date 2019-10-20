d3412c9934f3283d801ae5c21b6093fc3c66dc52
megajd16 <39034167+megajd16@users.noreply.github.com>
2019-10-20 18:03:25 -0500

    Tabla de pediatras con registro, y obtencion por lista

---
+++ app/src/main/java/com/example/mypediatric/Models/Pediatra.java
@@ -0,0 +1,67 @@
+package com.example.mypediatric.Models;
+
+public class Pediatra {
+    private String Uid;
+    private String Nombre;
+    private String Apellido;
+    private String Telefono;
+    private String Ubicacion;
+    private String Descripcion;
+
+    public Pediatra() {
+    }
+
+    public String getUid() {
+        return Uid;
+    }
+
+    public void setUid(String uid) {
+        Uid = uid;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setApellido(String apellido) {
+        Apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        Telefono = telefono;
+    }
+
+    public String getUbicacion() {
+        return Ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        Ubicacion = ubicacion;
+    }
+
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        Descripcion = descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return Nombre+" "+Apellido;
+    }
+
+}
